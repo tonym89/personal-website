f8ee5454ec0355247bc5d9b2b210ac93d6da3ca1
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-23 19:46:52 +0500

    Updates

---
+++ precache-manifest.e17ad08b468dac2613eb9c900e7ca784.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a62f60f91e093dc8a82bc81403d0aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490cf565d5f0184a8bbd",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b961c175b221ff9f9fc7",
+    "url": "/static/css/main.2ed97e1b.chunk.css"
+  },
+  {
+    "revision": "490cf565d5f0184a8bbd",
+    "url": "/static/js/2.f119b887.chunk.js"
+  },
+  {
+    "revision": "b961c175b221ff9f9fc7",
+    "url": "/static/js/main.cbe12f13.chunk.js"
+  },
+  {
+    "revision": "cd67f41765597a4534e6",
+    "url": "/static/js/runtime-main.a090d64f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "c8282e4bf63e55cd0e118e97f419f154",
+    "url": "/static/media/mainPhoto.c8282e4b.jpg"
+  }
+]);
